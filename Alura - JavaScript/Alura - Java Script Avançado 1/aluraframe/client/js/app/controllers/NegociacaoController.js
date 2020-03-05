12401c23384f762af7d1b4acbff7e79067dfc0a0
Samuel da Silva Rocha <T1090440@interno.bb.com.br>
2020-03-05 15:27:47 -0300

    retomando o curso de JavaScript avanced

--- Alura - JavaScript/Alura - Java Script Avançado 1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Alura - JavaScript/Alura - Java Script Avançado 1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,30 +1,26 @@
 class NegociacaoController {
     
-    constructor() {
-        
+    constructor(){
         let $ = document.querySelector.bind(document);
+
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
     }
-    
-    adiciona(event) {
-        
+
+    adiciona(event){
         event.preventDefault();
-      
-        let data = new Date(
-            ...this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
-            
+
+        let data = DateHelper.textoParaData(this._inputData.value);
+
         let negociacao = new Negociacao(
             data,
             this._inputQuantidade.value,
-            this._inputValor.value  
+            this._inputValor.value
         );
-        
+
         console.log(negociacao);
+        console.log(DateHelper.dataParaTexto(negociacao.data));
     }
 
     limparFormulario(){
